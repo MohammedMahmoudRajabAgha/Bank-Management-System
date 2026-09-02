a9d1b039fedcf27c9cd706f899cd570393cd97d2
Mohammed Mahmoed Rajeb Aga <Mohammad940367206@gmail.com>
2026-09-02 11:01:46 +0300

    Add Logout Register Choice To Main Menue Screen

--- clsMainScreen.h
+++ clsMainScreen.h
@@ -201,7 +201,8 @@ public:
         cout << setw(37) << left << "" << "\t[6] Transactions.\n";
         cout << setw(37) << left << "" << "\t[7] Manage Users.\n";
         cout << setw(37) << left << "" << "\t[8] Login Register.\n";
-        cout << setw(37) << left << "" << "\t[9] Logout.\n";
+        cout << setw(37) << left << "" << "\t[9] Logout Register.\n";
+        cout << setw(37) << left << "" << "\t[10] Logout.\n";
         cout << setw(37) << left << "" << "============================================\n";
 
         _PerfromMainMenueOption((enMainMenueOptions)_ReadMainMenueOption());
